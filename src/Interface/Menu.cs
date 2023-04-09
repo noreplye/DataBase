6e70605523c4484f2532cf63dfb370e02bd8375d
Svetlana Kharchenko <durnfogus@gmail.com>
2023-04-09 08:56:39 +0500

    Business Logic Directory, Interface

---
+++ src/Interface/Menu.cs
@@ -0,0 +1,19 @@
+﻿namespace DataBase.Interface;
+
+public class Menu: IDrawable
+{
+    public Menu()
+    {
+    }
+        
+
+    public void Run()
+    {
+        Draw();
+    }
+
+    private void Draw()
+    {
+        
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace DataBase
+﻿using DataBase.Interface;
+
+namespace DataBase
 {
     class Program
     {
         private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            Menu menu = new Menu();
+            
+            menu.Run();
         }
     }   
 }
