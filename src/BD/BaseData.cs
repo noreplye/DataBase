65a89cd15807642a251a9b07e635b52787e4b8e3
t00sh0rt <maksimfpm28@gmail.com>
2023-04-13 18:52:41 +1000

    Пока что какую-то фигню сделал класс для базы данных и json для комнат

---
+++ src/BD/BaseData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.BD
+{
+
+    public class RoomsArray
+    {
+        public Room[] rooms { get; set; }
+    }
+
+    public class Room
+    {
+        public string Name { get; set; }
+        public string Quality { get; set; }
+        public string Description { get; set; }
+        public int Seats { get; set; }
+        public int RoomPrice { get; set; }
+        public int Room_id { get; set; }
+    }
+
+}
