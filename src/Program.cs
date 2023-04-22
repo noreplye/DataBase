9ed26def726a35a525fb034b710d98151fb50733
BojeUbeyteMenya <mralexgold01@gmail.com>
2023-04-22 21:15:49 +1000

    сокеты клиента

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,55 @@
 ﻿using DataBase.Interface;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace DataBase
 {
     class Program
     {
-        private static void Main()
+        public static void ServerMessage(string message)
         {
-            Console.WriteLine("Hello, World!");
+            const string ip = "127.0.0.1"; //Ip локальный
+            const int port = 8080; //Port любой
+            while (true)
+            {
 
-            Menu menu = new Menu();
+                var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port); // класс конечной точки (точка подключения), принимает Ip and Port
+
+                var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // сокет объявляем, через него все проходит + прописываем дефолтные характеристики для TCP
+
+                var data = Encoding.UTF8.GetBytes(message);
+                tcpSocket.Connect(tcpEndPoint);
+                tcpSocket.Send(data);
+
+                var buffer = new byte[256];
+                var size = 0;
+                var answer = new StringBuilder();
+
+
+                do
+                {
+                    size = tcpSocket.Receive(buffer);
+                    answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                }
+                while (tcpSocket.Available > 0);
+
+                Console.WriteLine(answer.ToString());
+                break;
+            }
+        }
+        static void Main(string[] args)
+        {
             
-            menu.Run();
+
+                Menu menu = new Menu();
+
+                menu.Run();
+            string gg = Console.ReadLine();
+            ServerMessage (gg);
+            gg = Console.ReadLine();
+            ServerMessage(gg);
+            Console.ReadLine();
         }
     }   
 }
