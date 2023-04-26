8fb3319b5a96e550b6ffdc747c5ed911194d38d4
BojeUbeyteMenya <mralexgold01@gmail.com>
2023-04-26 18:42:42 +1000

    я вставил код Сережи и подредачил код, чтобы на серваке принимало и решало че делать

--- src/BusinessLogic/BusinessLogic.cs
+++ src/BusinessLogic/BusinessLogic.cs
@@ -6,7 +6,47 @@ namespace DataBase.BL;
 
 public class BusinessLogic
 {
-    public static string ServerMessage(string message)
+    public static string ServerMessage(int choice, string message)
+    {
+        const string ip = "127.0.0.1"; //Ip локальный
+        const int port = 8080; //Port любой
+        while (true)
+        {
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port); // класс конечной точки (точка подключения), принимает Ip and Port
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // сокет объявляем, через него все проходит + прописываем дефолтные характеристики для TCP
+
+            byte[] bytesA = BitConverter.GetBytes(choice);
+
+
+
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            byte[] BytesToSend = bytesA.Concat(data).ToArray();
+
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(BytesToSend);
+
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+            
+
+            do
+            {
+                size = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            }
+            while (tcpSocket.Available > 0);
+
+
+            return answer.ToString();
+            break;
+        }
+    }
+
+    public static string ServerChoose(string message)
     {
         const string ip = "127.0.0.1"; //Ip локальный
         const int port = 8080; //Port любой
@@ -38,7 +78,10 @@ public class BusinessLogic
             break;
         }
     }
+    public static void UserLogin(string UserLogin, string UserPassword)
+    {
 
+    }
 
     //console.writeline("пришли мне: \n1 - комнаты\n2 - клиентов");
     //choose = console.readline();
--- src/Program.cs
+++ src/Program.cs
@@ -2,43 +2,13 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using DataBase.BL;
 
 namespace DataBase
 {
     class Program
     {
-        public static string ServerMessage(string message)
-        {
-            const string ip = "127.0.0.1"; //Ip локальный
-            const int port = 8080; //Port любой
-            while (true)
-            {
-
-                var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port); // класс конечной точки (точка подключения), принимает Ip and Port
-
-                var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // сокет объявляем, через него все проходит + прописываем дефолтные характеристики для TCP
-
-                var data = Encoding.UTF8.GetBytes(message);
-                tcpSocket.Connect(tcpEndPoint);
-                tcpSocket.Send(data);
-
-                var buffer = new byte[256];
-                var size = 0;
-                var answer = new StringBuilder();
-
-
-                do
-                {
-                    size = tcpSocket.Receive(buffer);
-                    answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
-                }
-                while (tcpSocket.Available > 0);
-
-
-                return answer.ToString();
-                break;
-            }
-        }
+        
         static void Main(string[] args)
         {
             
@@ -46,11 +16,13 @@ namespace DataBase
                 Menu menu = new Menu();
 
                 menu.Run();
-            string gg = Console.ReadLine();
-            gg = ServerMessage (gg);
-            Console.WriteLine(gg);
-            gg = Console.ReadLine();
-            ServerMessage(gg);
+            while (true)
+            {
+                int gg = Convert.ToInt32(Console.ReadLine());
+                string gg1 = "Пиздец";
+                gg1 = DataBase.BL.BusinessLogic.ServerMessage(gg, gg1);
+                Console.WriteLine(gg1);
+            }
             Console.ReadLine();
         }
     }   
